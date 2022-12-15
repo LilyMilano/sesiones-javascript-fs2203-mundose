5dfde8043797ad94b19a4a0bf2217f761451ec0c
Liliana Milano <milanoliliana129@gmail.com>
2022-12-15 18:37:22 -0400

    [FEATURE] Add 'Array of Objects': .html and .js files

---
+++ array-de-objetos/array-objetos.js
@@ -0,0 +1,21 @@
+//Definiendo un array de Objetos.
+
+const personas = [
+    {
+        nombre: "Lucía",
+        apellido: "Disandro",
+        edad: 27,
+        programadora: true,
+        colorFavorito: ['celeste"','rosa'],
+        img: 'img/imagen.jpg'
+    },
+    {
+        nombre: "Lautaro",
+        apellido: "Montaña",
+        edad: 30,
+        programadora: false,
+        colorFavorito: "rojo"
+    },
+];
+
+console.log(personas[0]);
