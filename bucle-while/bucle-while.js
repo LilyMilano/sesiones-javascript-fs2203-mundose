f8181909b1090f4bd7b5745d58c088207c530023
Liliana Milano <milanoliliana129@gmail.com>
2022-12-15 14:57:03 -0400

    [FEATURE] Add 'While Loop': .html and .js files

---
+++ bucle-while/bucle-while.js
@@ -0,0 +1,17 @@
+//First declare the counter
+
+let counter = 1;
+
+//Then we write the reserved word While
+
+while( counter <= 10 ){
+    
+    //Backticks ``
+    
+    console.log(`Iteracion ${counter}`)
+
+    counter++
+}
+
+
+
