145eebe2afed1c24092ec663b4ec534febb4236b
Liliana Milano <milanoliliana129@gmail.com>
2022-12-15 19:36:23 -0400

    [FEATURE] Add 'Basic Functions': .html and .js files

---
+++ funciones-basicas/funciones.js
@@ -0,0 +1,9 @@
+// Funciones: Instrucciones anidadas
+let nombre = prompt("Ingresar nombre");
+
+function saludar(nombre){
+document.write('Te damos la bienvenida ' + nombre + `<h1>Disfruta tu viaje</h1>` + `<br>`)
+document.write('Te damos la bienvenida ' + nombre)
+};
+
+saludar(nombre);
