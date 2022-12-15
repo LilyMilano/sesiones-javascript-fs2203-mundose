d5304360332fc942c5da4d7866c0ed9522d1b83c
Liliana Milano <milanoliliana129@gmail.com>
2022-12-15 16:48:08 -0400

    [FEATURE] Add 'Objects': .html and .js files

---
+++ objetos/objetos.js
@@ -0,0 +1,18 @@
+//Para definir un objeto:
+const persona = {
+    // Clave | Valor
+    nombre: "Lucía",
+    apellido: "Disandro",
+    edad: 27,
+    programadora: true,
+    colorFavorito: "celeste"
+};
+
+console.log(persona);
+console.log(persona.nombre);
+
+// dot.notation:
+persona.nombre = "Lautaro";
+
+// bracket.notation:
+persona["colorFavorito"] = "Verde";
